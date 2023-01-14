c26ce5b2effd8104f63823d7cb8d9ac1dd7c2fcb
axkeyz <xahkun@gmail.com>
2023-01-14 21:09:42 +1300

    Update .gitignore

--- sidebar/panel.js
+++ sidebar/panel.js
@@ -3,3 +3,10 @@ document.getElementById("transfer").addEventListener("change", enableCopy)
 function enableCopy() {
     document.getElementById("copy").disabled = false;
 }
+
+select = document.getElementById("transfer"); 
+options = browser.storage.local.get("sites")
+
+for(i in options){
+    select.add(new Option(options[i].toUpperCase().replace("-", " -> "), options[i]), undefined)
+}
